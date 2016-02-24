349cf42cc542cc915193f7f6d48404f92d397c3a
rxm <rezwanam@gmail.com>
2016-02-23 18:32:26 -0800

    diagonostics logging

---
+++ app/src/main/java/com/meerkats/familyshopper/SettingsLoggingActivity.java
@@ -0,0 +1,56 @@
+package com.meerkats.familyshopper;
+
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.meerkats.familyshopper.util.Settings;
+
+public class SettingsLoggingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(Settings.getColorTheme());
+        setContentView(R.layout.settings_with_toolbar);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.settings_toolbar);
+        setSupportActionBar(myToolbar);
+        myToolbar.setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setTitle("Settings");
+
+        getFragmentManager().beginTransaction().replace(R.id.settings_frame, new MyPreferenceFragment()).commit();
+        PreferenceManager.setDefaultValues(this, R.xml.settings_logging, false);
+
+    }
+
+    @Override
+    protected void onApplyThemeResource(Resources.Theme theme, int resid, boolean first) {
+
+        theme.applyStyle(Settings.getColorTheme(), true);
+    }
+
+
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_logging);
+        }
+
+        @Override
+        public void onPause(){
+            super.onPause();
+        }
+
+    }
+
+
+
+}
--- app/src/main/java/com/meerkats/familyshopper/util/DiagnosticsActivity.java
+++ app/src/main/java/com/meerkats/familyshopper/util/DiagnosticsActivity.java
@@ -1,19 +1,25 @@
 package com.meerkats.familyshopper.util;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import com.meerkats.familyshopper.MainController;
 import com.meerkats.familyshopper.R;
+import com.meerkats.familyshopper.SettingsLoggingActivity;
 import com.meerkats.familyshopper.util.Settings;
 
 /**
  * Created by Rez on 23/02/2016.
  */
 public class DiagnosticsActivity extends AppCompatActivity {
+    private static final int DIAGONOSTICS_SETTINGS_RESULT = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,4 +43,24 @@ public class DiagnosticsActivity extends AppCompatActivity {
         listView.setAdapter(simpleAdapter);
 
     }
+
+    public void diagnosticsSettingsClick(View view){
+        Intent intent = new Intent(this, SettingsLoggingActivity.class);
+        startActivityForResult(intent, DIAGONOSTICS_SETTINGS_RESULT);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case DIAGONOSTICS_SETTINGS_RESULT:
+                Settings.loadSettings(this);
+                //notify service that settings have changed
+                Intent intent = new Intent(MainController.settings_changed_action);
+                LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+        }
+    }
+
+
 }
